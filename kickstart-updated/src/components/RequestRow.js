8d685d2045f273804bb67506b9d312547b31cc75
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-02-19 20:04:10 +0530

    Completed Course

---
+++ kickstart-updated/src/components/RequestRow.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Table,Button } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
+import Campaign from '../../ethereum/campaign';
+export default class RequestRow extends Component {
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0],
+    });
+  };
+    onFinalize = async () => { 
+        const campaign = Campaign(this.props.address);
+        const accounts = await web3.eth.getAccounts();
+        await campaign.methods.finalizeRequest(this.props.id).send({
+            from: accounts[0],
+        })
+    }
+  render() {
+    const { Row, Cell } = Table;
+    const { id, request, approversCount } = this.props;
+
+    return (
+      <Row>
+        <Cell>{id}</Cell>
+        <Cell>{request.description}</Cell>
+        <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
+        <Cell>{request.recipient}</Cell>
+        <Cell>
+          {request.approvalCount}/{approversCount}
+        </Cell>
+            <Cell>
+                
+          <Button color="green" basic onClick={this.onApprove}>
+            Approve
+          </Button>
+        </Cell>
+        <Cell>
+          <Button color="teal" basic onClick={this.onFinalize}>
+            Finalize
+          </Button>
+        </Cell>
+        {/* <Cell>{request}</Cell>
+        <Cell>{request}</Cell>
+        <Cell>{request}</Cell>
+        <Cell>{request}</Cell> */}
+      </Row>
+    );
+  }
+}
--- kickstart-updated/src/pages/campaigns/requests/index.js
+++ kickstart-updated/src/pages/campaigns/requests/index.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button,Table } from 'semantic-ui-react';
 import { Link } from '../../../../routes';
-import Layout from '../../../components/Layout';
+import Layout from '../../../components/Layout'; 0
+import Campaign from '../../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
+
 class RequestIndex extends Component {
     static async getInitialProps(props) { 
-        const { address } = props.query;
-        return {address};
-    }
+      const { address } = props.query;
+      const campaign = Campaign(address);
+      const requestsCount = await campaign.methods.getRequestsCount().call();
+      const approversCount = await campaign.methods.approversCount().call();
+      const requests = await Promise.all(
+        Array(parseInt(requestsCount))
+          .fill()
+          .map((element, index) => {
+            return campaign.methods.requests(index).call();
+          })
+      );
+     
+      return { address, requests, requestsCount, approversCount };
+  }
+  renderRow() { 
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          request={request}
+          key={index}
+          id={index}
+          approversCount={this.props.approversCount}
+          address={this.props.address}
+        />
+      );
+    });
+  }
   render() {
+    const { Header,Row,HeaderCell,Body} = Table;
     return (
       <Layout>
         <h3>Requests</h3>
-            <Link route={`/campaigns/${this.props.address}/requests/new` }>
-                <a >
-                
-                <Button primary>Add Request</Button>
-                
-                </a>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+            {this.renderRow()}
+          </Body>
+        </Table>
       </Layout>
     );
   }
