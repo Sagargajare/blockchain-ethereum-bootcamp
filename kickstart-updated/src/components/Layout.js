29f4bf99e7d7256a44fb3515cdc9283ba6bf5646
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-02-19 22:01:47 +0530

    added notes for user

--- kickstart-updated/src/components/Layout.js
+++ kickstart-updated/src/components/Layout.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { Container} from 'semantic-ui-react'
 import Header from './Header';
 import Head from "next/head";
+import { Menu } from 'semantic-ui-react';
 const Layout = (props) => {
   return (
     <Container>
       <Head>
         <link
-        async
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-      />
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
       </Head>
       <Header />
       {props.children}
+      <Menu
+        style={{
+          marginTop: '20px',
+        }}
+      >
+        <Menu.Item color="red"> Connect to Ethereum Rinkeby Network with Metamask</Menu.Item>
+      </Menu>
     </Container>
   );
 };
